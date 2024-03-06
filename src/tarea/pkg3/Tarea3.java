9c9f7de8e56d38a4abcacf36a0c66a001cbbf40c
famil <famil@LAPTOP-M8631B1T>
2024-03-06 15:01:17 -0600

    Cargando...

--- src/tarea/pkg3/Tarea3.java
+++ src/tarea/pkg3/Tarea3.java
@@ -45,7 +45,7 @@ class Pentagono extends Tarea3 {
 
     @Override
     public String getNombre() {
-        return "Pentágono";
+        return "Pentagono";
     }
 }
 
@@ -87,7 +87,7 @@ class Triangulo extends Tarea3 {
 
     @Override
     public String getNombre() {
-        return "Triángulo";
+        return "Triangulo";
     }
 }
 
@@ -95,30 +95,41 @@ class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = "";
 
-        System.out.println("Por favor, introduce el numero de lados de la figura 3 para un triangulo, 4 para un cuadrado, 5 para un pentagono):");
-        int numeroDeLados = scanner.nextInt();
+        while (!input.equalsIgnoreCase("salir")) {
+            System.out.println("Escribir 'salir' para Salir");
+            System.out.println("Por favor, introduce el numero de lados de la figura 3 para un triangulo, 4 para un cuadrado, 5 para un pentagono:");
+            input = scanner.nextLine();
+            
+            if (input.equalsIgnoreCase("salir")) {
+                break;
+            }
+            
+            int numeroDeLados = scanner.nextInt();
 
-        System.out.println("Por favor, introduce el valor del lado o la base y la altura para el triangulo");
-        double valor1 = scanner.nextDouble();
-        double valor2 = (numeroDeLados == 3) ? scanner.nextDouble() : valor1;
+            System.out.println("Por favor, introduce el valor del lado o la base y la altura para el triangulo");
+            double valor1 = scanner.nextDouble();
+            double valor2 = (numeroDeLados == 3) ? scanner.nextDouble() : valor1;
 
-        Tarea3 figura;
-        switch (numeroDeLados) {
-            case 3:
-                figura = new Triangulo(valor1, valor2);
-                break;
-            case 4:
-                figura = new Cuadrado(valor1);
-                break;
-            case 5:
-                figura = new Pentagono(valor1);
-                break;
-            default:
-                System.out.println("Número de lados no válido.");
-                return;
+            Tarea3 figura;
+            switch (numeroDeLados) {
+                case 3:
+                    figura = new Triangulo(valor1, valor2);
+                    break;
+                case 4:
+                    figura = new Cuadrado(valor1);
+                    break;
+                case 5:
+                    figura = new Pentagono(valor1);
+                    break;
+                default:
+                    System.out.println("Numero de lados no valido.");
+                    return;
+            }
+
+            figura.imprimirArea();
+            scanner.nextLine();
         }
-
-        figura.imprimirArea();
     }
 }
